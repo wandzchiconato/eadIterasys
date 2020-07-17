48372263261e1c5f58f6874fad75864da03a8b8e
wandzchiconato <wandz@outlook.com>
2020-07-16 22:44:38 -0300

    Adicionado classes para Automação

--- src/test/java/Steps/pesquisa.java
+++ src/test/java/Steps/pesquisa.java
@@ -1,45 +1,102 @@
 package Steps;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 
+public class pesquisa {
+    String url;
+    WebDriver driver;
+    static String caminhoPrint = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
+    public void print(String nomePrint) throws IOException, IOException {
 
-public class pesquisa  {
+        File foto = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(foto, new File("out/prints/" + caminhoPrint + "/" + nomePrint + ".png"));
+    }
+    @Before
+    public void Iniciar() {
+        url = "https://www.iterasys.com.br";
+        System.setProperty("webdriver.chrome.driver","driver/chrome/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+
+    }
+
+    @After
+    public void finalizar() {
+        driver.quit();
+
+    }
 
     @Given("^que o visitante acessa o site da Iterasys$")
     public void que_o_visitante_acessa_o_site_da_Iterasys() throws Throwable {
-        System.out.println("Passo 1");
+        driver.get(url);
+
+        print("Consultar Curso - Passo 1 - Abriu o site");
+        System.out.println("Passo 1 - Acessou o Site");
+
     }
 
     @Given("^preenche o termo de busca como \"([^\"]*)\"$")
-    public void preenche_o_termo_de_busca_como(String arg1) throws Throwable {
-        System.out.println("Passo 2");
+    public void preenche_o_termo_de_busca_como(String termo) throws Throwable {
+        driver.findElement(By.id("searchtext")).click();
+        driver.findElement(By.id("searchtext")).clear();
+        driver.findElement(By.id("searchtext")).sendKeys(termo);
+        print("Consultar Curso - Passo 2 - Digitou a pesquisa");
+        System.out.println("Passo 2 - Digitou a pesquisa");
     }
 
     @Given("^clica no botao Lupa$")
     public void clica_no_botao_Lupa() throws Throwable {
-        System.out.println("Passo 3");
+        driver.findElement(By.id("btn_form_search")).click();
+        print("Consultar Curso - Passo 3 - Clicou em Procurar");
+        System.out.println("Passo 3 - Clicou em procurar");
     }
 
-    @Then("^visualiza a lista de cursos relacionados$")
-    public void visualiza_a_lista_de_cursos_relacionados() throws Throwable {
-        System.out.println("Passo 4");
+    @Then("^visualiza a lista de cursos relacionados a \"([^\"]*)\"$")
+    public void visualiza_a_lista_de_cursos_relacionados(String termo) throws Throwable {
+        //Assert.assertEquals(" Cursos › " + termo  ,driver.findElement(By.cssSelector("col-md-6")).getText());
+        //Assert.assertTrue(driver.findElement(By.tagName("h3")).getText().contains(termo));
+        Assert.assertEquals("Cursos"+ termo,driver.findElement(By.tagName("h3")).getText().replaceAll("[^a-zA-Z0-9]", ""));
+        print("Consultar Curso - Passo 4 - Validou o nome do curso");
+        System.out.println("Passo 4 - Validou o nome do curso");
     }
 
     @When("^seleciona o curso \"([^\"]*)\" na lista$")
-    public void seleciona_o_curso_na_lista(String arg1) throws Throwable {
-        System.out.println("Passo 5");
+    public void seleciona_o_curso_na_lista(String termo) throws Throwable {
+        driver.findElement(By.cssSelector("span.comprar")).click();
+        print("Consultar Curso - Passo 5 - Selecionou o curso");
+        System.out.println("Passo 5 - Clicou em Comprar ");
     }
 
     @Then("^visualiza o titulo como \"([^\"]*)\" e o preco como \"([^\"]*)\"$")
-    public void visualiza_o_titulo_como_e_o_preco_como(String arg1, String arg2) throws Throwable {
-        System.out.println("Passo 6");
+    public void visualiza_o_titulo_como_e_o_preco_como(String titulo, String preco) throws Throwable {
+        Assert.assertEquals(titulo,driver.findElement(By.cssSelector("span.item-title")).getText());
+        Assert.assertEquals(preco,driver.findElement(By.cssSelector("span.new-price")).getText());
+        print("Consultar Curso - Passo 6 - Validou o curso e o Preço");
+        System.out.println("Passo 6 - Validou o Curso e o Preço");
+    }
+    @And("^aperta o enter$")
+    public void aperta_o_enter() throws Throwable {
+        driver.findElement(By.id("btn_form_search")).sendKeys(Keys.ENTER);
     }
 
 }
--- src/test/resources/Features/pesquisa.feature
+++ src/test/resources/Features/pesquisa.feature
@@ -1,9 +1,31 @@
 Feature: Busca Curso
-  Scenario: Efetuar a busca de um curso
+  Scenario: Efetuar a busca de um curso clicando na lupa
     Given que o visitante acessa o site da Iterasys
     And preenche o termo de busca como "Mantis"
     And clica no botao Lupa
-    Then visualiza a lista de cursos relacionados
+    Then visualiza a lista de cursos relacionados a "Mantis"
     When seleciona o curso "Mantis" na lista
     Then visualiza o titulo como "Mantis" e o preco como "R$ 49,99"
 
+  Scenario: Efetuar a busca de um curso apertando enter
+    Given que o visitante acessa o site da Iterasys
+    And preenche o termo de busca como "TestLink"
+    And aperta o enter
+    Then visualiza a lista de cursos relacionados a "TestLink"
+    When seleciona o curso "TestLink" na lista
+    Then visualiza o titulo como "TestLink" e o preco como "R$ 79,99"
+
+  Scenario Outline: Busca por Curso Simples com Clique utilizando Exemplos
+
+    # Multiplas Linhas
+  Given que o visitante acessa o site da Iterasys
+  And preenche o termo de busca como <termo>
+  And clica no botao Lupa
+  Then visualiza a lista de cursos relacionados a <termo>
+  When seleciona o curso <titulo> na lista
+  Then visualiza o titulo como <titulo> e o preco como <preco>
+
+  Examples:
+  | termo    | titulo       | preco      |
+  | "MANTIS"   | "Mantis"   | "R$ 49,99" |
+  | "testlink" | "TestLink" | "R$ 79,99" |
